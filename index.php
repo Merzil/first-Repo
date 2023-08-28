25f5a2cb4b350e40c22d3314473b5d5e13e2684f
Merzil <120817335+Merzil@users.noreply.github.com>
2023-08-28 20:47:21 +0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+  
+      
+  </head>
+  <body>
+    <h1>My first Repository</h1>
+    <ul>
+      <li>Tea</li>
+      <li>Tea</li>
+      <li>Tea</li></ul>
+    
+  </body>
+</html>
